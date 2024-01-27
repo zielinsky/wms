9ad719cd32995116f0c6af5ba4d0f2df72df5b90
Patryk Zieliński <mrzbiznes@gmail.com>
2024-01-27 15:40:43 +0100

    add client side to firebase

--- client/firebase.ts
+++ client/firebase.ts
@@ -1,10 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
 
-let serviceAccount = require('./creds.json')
-initializeApp()
+const firebaseConfig = {
+    apiKey: "AIzaSyBlFOIOW7Ip1V-BpGFH9FbmAvX2Kd_LqQc",
+    authDomain: "wms-ii.firebaseapp.com",
+    projectId: "wms-ii",
+    storageBucket: "wms-ii.appspot.com",
+    messagingSenderId: "779284821070",
+    appId: "1:779284821070:web:0791f786e4d52727a73f1f",
+    measurementId: "G-6JDNFEZ1CP"
+  };
+
+initializeApp(firebaseConfig)
 
 const auth = getAuth() 
-
 
 export { auth }
