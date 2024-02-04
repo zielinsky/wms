84762d28e23da148cc37391a0cb831a1e373411c
Patryk Zieliński <mrzbiznes@gmail.com>
2024-02-04 19:05:45 +0100

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import UserRoutes from "./routers/users.router";
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
 export const app: Express = express();
 const port = 3000;
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
